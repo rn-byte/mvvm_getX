09838443317fd8de46dec1c87f0b97ea20a40c61
rn-byte <ramthakur977+github@gmail.com>
2024-07-07 23:25:21 +0545

    Lifetime resuable button compenent added

---
+++ lib/res/components/round_button.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:mvvm_getx_flutter/res/colors/app_colors.dart';
+
+class RoundButton extends StatelessWidget {
+  final bool loading;
+  final String title;
+  final double height, width;
+  final VoidCallback onPress;
+  final Color textColor, buttonColor;
+
+  const RoundButton({
+    super.key,
+    this.buttonColor = AppColors.primaryButtonColor,
+    this.textColor = AppColors.primaryTextColor,
+    required this.title,
+    required this.onPress,
+    this.width = 60,
+    this.height = 50,
+    this.loading = false,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: height,
+      width: width,
+      decoration: BoxDecoration(
+        color: buttonColor,
+        borderRadius: BorderRadius.circular(50),
+      ),
+      child: loading
+          ? const Center(child: CircularProgressIndicator())
+          : Center(
+              child: Text(
+                title,
+                style: Theme.of(context)
+                    .textTheme
+                    .titleMedium!
+                    .copyWith(color: Colors.white),
+              ),
+            ), // Center
+    ); // Container
+  }
+}
--- lib/views/splash_screen.dart
+++ lib/views/splash_screen.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 // ignore: unused_import
 import 'package:mvvm_getx_flutter/res/asset/image_assets.dart';
+import 'package:mvvm_getx_flutter/res/colors/app_colors.dart';
+// ignore: unused_import
 import 'package:mvvm_getx_flutter/res/components/internet_exception_widgets.dart';
+import 'package:mvvm_getx_flutter/res/components/round_button.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -15,8 +18,39 @@ class _SplashScreenState extends State<SplashScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: InternetExceptionWidgets(
-          onPress: () {},
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            RoundButton(
+              onPress: () {},
+              title: 'Login',
+              width: 200,
+              buttonColor: AppColors.redColor,
+              loading: false,
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            RoundButton(
+              onPress: () {},
+              title: 'Signup',
+              width: 250,
+              height: 40,
+              buttonColor: AppColors.primaryButtonColor,
+              loading: false,
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            RoundButton(
+              onPress: () {},
+              title: 'Apply',
+              width: 350,
+              height: 50,
+              buttonColor: AppColors.secondaryButtonColor,
+              loading: true,
+            )
+          ],
         ),
       ),
     );
